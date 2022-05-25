9f2b11c21d781e8759db0bd1d1f591ade164ee38
Claire Sersun <claire.sersun@gmail.com>
2022-05-24 23:33:26 -0400

    show correct letter when entered

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Variables
 var words = [
   'bananas',
   'grapes',
@@ -11,9 +12,50 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var remainingGuesses = 10
+var incorrectLetters = []
+var lettersGuessed = []
+var losses
+var wins
+
+
 var randomWord = words[Math.floor(Math.random()*words.length)]
 
 var hiddenWord = randomWord.replace(/[a-z]/g, '_')
 
+// DOM Manipulation
 document.querySelector("#word-to-guess").innerHTML = hiddenWord
+
+document.querySelector("#remaining-guesses").innerHTML = remainingGuesses
+
+document.querySelector("#incorrect-letters").innerHTML = incorrectLetters
+
+window.addEventListener('keydown', function (e) {
+  debugger
+  console.log(e.key);
+  var keystroke = e.key
+  //check that it is a letter
+  if (keystroke >= 'a' && keystroke <= 'z')
+  //check if letter already named
+    if (lettersGuessed.indexOf(keystroke) === -1) {
+      lettersGuessed.push(keystroke);
+      //if char is in randomWord
+      if(randomWord.indexOf(keystroke) !== -1) {
+        for(var i = 0; i<randomWord.length; i++) {
+          if( randomWord.includes(keystroke)) {
+            document.querySelector("#word-to-guess").innerHTML = hiddenWord.replace(hiddenWord[i],keystroke)}
+        }
+        //check if won
+        if(randomWord === hiddenWord) {
+          wins++;
+        }
+      } else {
+        remainingGuesses--;
+        incorrectLetters.push(keystroke);
+        if(remainingGuesses === 0) {
+          losses++
+        }}}
+}, false);
+console.log(randomWord)
 
