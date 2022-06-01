89266ec882914d0fc3dce5634ce8035410414fd3
Claire Sersun <claire.sersun@gmail.com>
2022-05-31 21:13:24 -0400

    made updates

--- index.js
+++ index.js
@@ -12,122 +12,93 @@ var words = [
   'ukulele',
   'mango'
 ]
-var remainingGuesses = document.querySelector("#remaining-guesses")
-var guessesLeft = 10
-var incorrectLetters = document.querySelector("#incorrect-letters")
-var lettersWrongArr = []
-var lettersGuessed = []
-var losses
-var wins
 var wordToGuess = document.querySelector("#word-to-guess")
-//Random word from array is chosen
+var remainingGuesses = document.querySelector("#remaining-guesses")
+var incorrectLetters = document.querySelector("#incorrect-letters")
+var incorrectLettersArr = ['']
+var letterGuessed = ['']
+// targets the previous-word ID
+var previousWord = document.querySelector("#previous-word")
+// targets the wins ID
+var winDisplay = document.querySelector("#wins")
+//targets the losses ID
+var lossDisplay = document.querySelector("#losses")
+var wins = 0
+var losses = 0 
+var guessesLeft = 10
+// this will be an array that will be storing the correctLetters the user is typing in
+var correctLetters = ['']
 var randomWord = words[Math.floor(Math.random()*words.length)]
-var randomArr = new Array (randomWord.split(''))
-
-// Letters are replaced with underscores
 var hiddenWord = randomWord.replace(/[a-z]/g, '_')
-var hiddenArr = new Array (hiddenWord.split(''))
-      console.log(hiddenArr)
 
 
-// Initial DOM Manipulation
-// Word chosen is placed in #word-to-guess element
-wordToGuess.textContent = hiddenWord
-// Game displays 10 remaining guesses in #remaining-guesses element
-remainingGuesses.textContent = guessesLeft
-incorrectLetters.textContent = lettersWrongArr
-
-// User presses key
+// Onkeyup functionality
 document.onkeyup = function(e) {
-  console.log(e)
-  var key = e.key
-  // Check if letter is included in the random word
+  // Variable that will store the key pressed by the user, also making sure it is lowercase when it is stored.
+  var key = e.key.toLowerCase()
+  // Create if/elseif statement where if the right letter is pressed then we will push() that letter to the letterGuessed array. Else if the wrong letter is pressed, it will be pushed to the lettersWrongArr array. You would also subtract from the guessesLeft variable
   if (randomWord.indexOf(key) !== -1) {
-    // If included, replace all iterations of the underscore in with the letter in the #word-to-guess element
-    for (let i = 0; i < hiddenWord.length; i++) {
-//find index of char in randomWord
-var indexKeyInWord = randomWord.indexOf(key)
-console.log(indexKeyInWord)
-//replace same index with the key in hiddenWord
-wordToGuess = hiddenWord.replace(indexKeyInWord[i], key)
-//find a child within the text node
-// debugger
-// function replaceAt(hiddenWord, indexKeyInWord, key) {
-//   return (
-//     hiddenWord.substr(0, indexKeyInWord) + key + hiddenWord.substr(indexKeyInWord + key.length)
-//   );
-// }
-// function checkLetter() {
-//   let matchedLetters = randomWord.match(key);
-//   return replaceAt(hiddenWord, matchedLetters.indexKeyInWord, key);
-// }
-// console.log (checkLetter)
-// wordToGuess.childNodes = hiddenWord.replace(hiddenWord, indexKeyInWord, key)
-// hiddenWord.forEach(indexKeyInWord, key) {
-//   // var a = hiddenArr.split("");
-//   hiddenWord[indexKeyInWord] = key;
-//   // return hiddenWord.join("");}
-// } 
-// console.log(hiddenArr)
-
-// //return hidenArr.join('')
-// wordToGuess.textContent = hiddenWord
-    
-      // hiddenWord.replace(hiddenWord.charAt(i), key) 
-      // wordToGuess.textContent =      
-    
-    }
-    // If letter not included #word-to-guess element remains unchanged and incorrect letter is added to the #incorrect-letters element and the #remaining-guesses element shows one fewer ransoming guesses
+    letterGuessed.push(key)
   } else {
-    lettersWrongArr.push(key)
-    incorrectLetters.textContent = lettersWrongArr
+    incorrectLetters.push(key)
     guessesLeft--
-    remainingGuesses.textContent = guessesLeft
-
+    console.log(guessesLeft)
   }
-}
-// If the user repeats the letter, no changes to the game state
-// When no underscores remain in the #word-to-guess element (or they run out of guesses in which case they add to the lose number vs the win [make part of this a function, the one that both do, and insert it]), win displays an added number to the score, new random word chosen with underscores, incorrect letters is blank, remaining guesses show 10, #previous-word reads the previous word
+  // Run the updated guess information function
+  updateGuessInfo()
+  // Create if statement where if the displayed word matches the random word from the array, then you would add a win, change the previous word with the word that was just guessed, and then run the new game function to run a new game
+  if (wordToGuess === randomWord) {
+    wins++
+    previousWord = randomWord
+    //run the new game function to run a new game
+    newGame()
+  }
+  // Create an if statement where if the user runs out of guesses, then we would add to the loss variable, add the word to the previous word section, and then run a new game using the new game function created below.
+  if (guessesLeft = 0) {
+    losses++
+    previousWord = randomWord
+    //run the new game function to run a new game
+    newGame()
+  }}
+  
+  // Function for new game
+  var newGame = function () {
+    // This function will contain a variable that will retrieve a random word from the words array. This is what you have on line 24.
+    randomWord
+    // This function will also reset the variables for correct letters, incorrect letters, and remaining guesses.
+    correctLetters = []
+    incorrectLetters = []
+    incorrectLetters.textContent = incorrectLettersArr
+    guessesLeft = 10
+    remainingGuesses.textContent = guessesLeft
+    // Once a new game start (when this function runs) we will also be running the update guess information function
+    updateGuessInfo()
+  }
 
-
-//one idea is to make the random word an array. see if letter pressed is in the array and then replace the location in the hiddenWord array that is merged to look like a single word with the given letter
-// __.textContent = ___
-///___.addEventListener()
-//document.onkeyup = function(e) {
-//   console.log(e.key)
-// h2.textContent = e.key
-// }
-
-
-// TRY AGAIN TOMORROW
-// window.addEventListener('keydown', function (e) {
-//   debugger
-//   console.log(e.key);
-//   var keystroke = e.key
-//   //check that it is a letter
-//   if (keystroke >= 'a' && keystroke <= 'z')
-//   //check if letter already named
-//     if (lettersGuessed.indexOf(keystroke) === -1) {
-//       lettersGuessed.push(keystroke);
-//       //if char is in randomWord
-//       if(randomWord.indexOf(keystroke) !== -1) {
-//         for(var i = 0; i<randomWord.length; i++) {
-//           if( randomWord.includes(keystroke)) {
-//             //THIS IS ONLY REPLACING THE FIRST LETTER!
-//             document.querySelector("#word-to-guess").innerHTML = hiddenWord.replace(hiddenWord[i],keystroke)}
-//         }
-//         //check if won
-//         if(randomWord === hiddenWord) {
-//           wins++;
-//         }
-//       } else {
-//         remainingGuesses--;
-//         document.querySelector("#remaining-guesses").innerHTML = remainingGuesses;
-//         incorrectLetters.push(keystroke);
-//         document.querySelector("#incorrect-letters").innerHTML = incorrectLetters;
-//         if(remainingGuesses === 0) {
-//           losses++
-//         }}}
-// }, false);
-console.log(randomWord)
-
+  // Function for updating the guess information
+  var updateGuessInfo =  function updateGuessInfo() {
+    // This is the function that will be updating underscores on the front-end of the website
+    // A for loop will be in this function (similar to your line 47)
+    for (let i = 0; i < randomWord.length; i++) {
+      // In the for loop an if statement will be located where if the letter is in the word, then display that letter, else display an underscore.
+      if (randomWord.includes(letterGuessed)) {
+        debugger
+        //display that letter
+        wordToGuess.textContent = hiddenWord.replace('_', letterGuessed)
+        //hiddenWord.replace(document.onkeyup.key)
+        console.log(hiddenWord.replace('_', letterGuessed))
+      } else {
+        //display underscore
+          // Letters are replaced with underscores
+        hiddenWord
+        console.log(hiddenWord)
+        wordToGuess.textContent = hiddenWord
+      }
+    }    
+    // This function will also add the letters that the user has missed to the bottom (similar to line 84 in your file)
+    remainingGuesses.textContent = guessesLeft
+    incorrectLetters.textContent = incorrectLettersArr
+  }
+   
+  // At the end of the file run the new game function.
+  newGame()
